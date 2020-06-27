0194f23dc2519230b1b981f012f044daf79527c4
yoichiro <youichirokaneko@gmail.com>
2020-06-27 10:20:27 +0900

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:dbstatusreset')->everyMinute();
-        // weeklyOn(0, '12:00');
+        $schedule->command('command:dbstatusreset')->weeklyOn(0, '12:00');
     }
 
     /**
